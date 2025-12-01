64c81ea7aeb928edf9791607606efc64b6f70735
Abdannawwaf1 <abdannawwaf1@gmail.com>
2025-12-01 08:12:35 +0700

    add web icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     
     {{-- Custom CSS --}}
+    <link rel="icon" type="image/svg+xml" href="{{ asset('storage/download.svg') }}">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
